4cc1d1fdc759392f1ae61230927128f0f7d6afa0
Jos02378 <57058570+Jos02378@users.noreply.github.com>
2021-03-31 07:09:55 +0700

    add how to host and schedule a meeting

---
+++ Zoom_Section/Zoom_Manual_Children/how to host and schedule a meeting/index.js
@@ -0,0 +1,62 @@
+const navTrigger = document.querySelector('.navigation_trigger');
+const navPanel = document.querySelector('.navigation_panel');
+const navBackground = document.querySelector('.navigation_background');
+const navHeader = document.querySelector('.navigation');
+const navBox = document.querySelector('.navigation_box');
+const navDropDown = document.querySelector('.navigation_manuals');
+const navManualsList = document.querySelector('.navigation_manuals_list');
+const wrapper = document.querySelector('.wrapper');
+let first_manual_video_yt_container = document.querySelector(".manual_video_yt_container");
+
+const disableScrolling = () => {
+    const x = window.scrollX;
+    const y = window.scrollY;
+    window.onscroll = function () {
+        window.scrollTo(x, y);
+    };
+}
+
+const enableScrolling = () => {
+    window.onscroll = function () {};
+}
+
+navTrigger.addEventListener('click', () => {
+    disableScrolling();
+
+    navPanel.style.opacity = 1;
+    navPanel.style['pointer-events'] = 'unset';
+    setTimeout(() => {
+        navBox.style.transform = 'translateY(0)';
+    }, 200);
+})
+
+navBackground.addEventListener('click', () => {
+    if (!navBackground.classList.contains('dropped')) {
+        navBox.style.transform = 'translateY(-100%)';
+        setTimeout(() => {
+            navPanel.style.opacity = 0;
+            navPanel.style['pointer-events'] = 'none';
+        }, 400);
+        enableScrolling();
+    } else {
+        navManualsList.classList.remove('dropped');
+        navBackground.classList.remove('dropped');
+    }
+});
+
+navDropDown.addEventListener('click', () => {
+    navManualsList.classList.toggle('dropped');
+    navBackground.classList.toggle('dropped');
+})
+
+window.addEventListener('scroll', () => {
+    const scrollAmount = window.scrollY;
+
+    if (scrollAmount > 50) {
+        navHeader.classList.add('alternate');
+    } else {
+        navHeader.classList.remove('alternate');
+    }
+})
+
+first_manual_video_yt_container.style.marginBottom = "50px";
